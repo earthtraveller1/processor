3406af0bdd0272c0f5ca7747bbf039e1ef31eace
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-10-27 16:42:26 -0400

    Wrote a basic utility function to split strings.

---
+++ src/string_utils.cpp
@@ -0,0 +1,31 @@
+#include "string_utils.hpp"
+
+namespace neng {
+std::vector<std::string> split_string(std::string_view string,
+                                      std::string_view delimiter) {
+    std::string current_segment;
+    std::vector<std::string> segments;
+
+    for (int i = 0; i < string.size(); i++) {
+        if (i + delimiter.size() - 1 < string.size()) {
+            bool hit_delimiter = true;
+            for (int j = 0; j < delimiter.size(); j++) {
+                if (string[i + j] != delimiter[j]) {
+                    hit_delimiter = false;
+                }
+            }
+
+            if (hit_delimiter) {
+                i += delimiter.size();
+                segments.push_back(current_segment);
+                current_segment = "";
+                continue;
+            }
+        }
+
+        current_segment.push_back(string[i]);
+    }
+
+    return segments;
+}
+} // namespace neng
---
+++ src/string_utils.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace neng {
+std::vector<std::string> split_string(std::string_view string,
+                                      std::string_view delimiter);
+} // namespace neng
