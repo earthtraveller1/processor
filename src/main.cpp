71ca1ad023aa585502ecd11b45222e86c20a4fb7
earthtraveller1 <liangyuezhao7@gmail.com>
2024-11-20 18:05:42 -0500

    Used the components that were built to process some files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include "tests.hpp"
+#include "document.hpp"
+
+#include <fstream>
 
 using namespace std::literals::string_literals;
 namespace fs = std::filesystem;
@@ -31,11 +34,60 @@ int main(int argc, char **argv) {
         }
     }
 
+    if (!fs::exists(target_path)) {
+        std::cerr << "[ERROR]: target directory does not exist.\n";
+        return EXIT_FAILURE;
+    }
+
+    const auto template_path = target_path / "template.neng";
+    if (!fs::exists(template_path)) {
+        std::cerr << "[ERROR]: template.neng file does not exist. Make sure a "
+                     "template.neng file exists in "
+                  << target_path << '\n';
+        return EXIT_FAILURE;
+    }
+
+    using neng::DocumentTemplate;
+    using neng::Error;
+
+    const auto [document_template, error] = DocumentTemplate::from_file(template_path.string());
+    if (error != Error::OK) {
+        std::cerr << "[ERROR]: Failed to parse the template: " << error << '\n';
+        return EXIT_FAILURE;
+    }
+
     // Ensure the output directory exists
     fs::create_directory(output_path);
 
     for (const auto &file : fs::recursive_directory_iterator{target_path}) {
-        std::cout << "Found file " << file.path() << "\n";
+        using neng::Document;
+
+        const auto file_path = file.path();
+        if (file_path.extension() == ".md") {
+            const auto [document, error] = Document::parse_document_from_file(file_path.string());
+            if (error != Error::OK) {
+                std::cerr << "[ERROR]: Failed to process document " << file_path
+                          << '\n';
+                continue;
+            }
+            
+            const auto result = document_template.render_html_to_string(document);
+            const auto file_output =
+                output_path / fs::relative(file_path, target_path).concat(".html");
+
+            auto file_output_dir = file_output;
+            file_output_dir.remove_filename();
+
+            fs::create_directories(file_output_dir);
+
+            std::ofstream output_file(file_output);
+            if (!output_file.is_open()) {
+                std::cerr << "[ERROR]: Failed to open " << file_output << " for writing.\n";
+                continue;
+            }
+
+            output_file << result;
+        }
     }
 
     return 0;
