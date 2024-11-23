e3e3f6c6c21d9c53ec1b5659684b6a6b9fee9735
EarthTraveller1 <liangyuezhao7@gmail.com>
2024-11-23 12:24:12 -0500

    Several changes - Formatted the document. - Integrated the slotted templating system for rendering templates. - Changed the names to match the new system.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
-#include "tests.hpp"
 #include "document.hpp"
+#include "tests.hpp"
 
 #include <fstream>
 
 using namespace std::literals::string_literals;
 namespace fs = std::filesystem;
 
-namespace {
-} // namespace
+namespace {} // namespace
 
 int main(int argc, char **argv) {
     fs::path target_path{"./pages"};
@@ -16,12 +15,12 @@ int main(int argc, char **argv) {
     for (char **arg = argv + 1; arg < argv + argc; arg++) {
         std::string_view sw_arg{*arg};
 
-        #ifdef PROCESSOR_BUILD_TESTS
+#ifdef PROCESSOR_BUILD_TESTS
         if (sw_arg == "--test") {
             neng::run_tests();
             return EXIT_SUCCESS;
         }
-        #endif
+#endif
 
         if (arg > argv + 1) {
             std::string_view previous_arg{*(arg - 1)};
@@ -39,10 +38,18 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    const auto template_path = target_path / "template.neng";
+    const auto config_path = target_path / "config.neng";
+    if (!fs::exists(config_path)) {
+        std::cerr << "[ERROR]: config.neng file does not exist. Make sure a "
+                     "config.neng file exists in "
+                  << target_path << '\n';
+        return EXIT_FAILURE;
+    }
+
+    const auto template_path = target_path / "template.html";
     if (!fs::exists(template_path)) {
-        std::cerr << "[ERROR]: template.neng file does not exist. Make sure a "
-                     "template.neng file exists in "
+        std::cerr << "[ERROR]: template.html file does not exist. Make sure a "
+                     "template.html file exists in "
                   << target_path << '\n';
         return EXIT_FAILURE;
     }
@@ -50,8 +57,17 @@ int main(int argc, char **argv) {
     using neng::DocumentConfiguration;
     using neng::Error;
 
-    const auto [document_template, error] = DocumentConfiguration::from_file(template_path.string());
+    const auto [document_config, error] =
+        DocumentConfiguration::from_file(config_path.string());
     if (error != Error::OK) {
+        std::cerr << "[ERROR]: Failed to parse the configuration: " << error
+                  << '\n';
+        return EXIT_FAILURE;
+    }
+
+    const auto [document_template, error2] =
+        neng::BasicDocumentTemplate::from_file(template_path);
+    if (error2 != Error::OK) {
         std::cerr << "[ERROR]: Failed to parse the template: " << error << '\n';
         return EXIT_FAILURE;
     }
@@ -64,16 +80,22 @@ int main(int argc, char **argv) {
 
         const auto file_path = file.path();
         if (file_path.extension() == ".md") {
-            const auto [document, error] = Document::parse_document_from_file(file_path.string());
+            const auto [document, error] =
+                Document::parse_document_from_file(file_path.string());
             if (error != Error::OK) {
                 std::cerr << "[ERROR]: Failed to process document " << file_path
                           << '\n';
                 continue;
             }
-            
-            const auto result = document_template.render_html_to_string(document);
+
+            const auto rendered_result =
+                document_config.render_html_to_string(document);
+            const auto slotted_result =
+                document_template.insert_body(rendered_result);
+
             const auto file_output =
-                output_path / fs::relative(file_path, target_path).concat(".html");
+                output_path /
+                fs::relative(file_path, target_path).concat(".html");
 
             auto file_output_dir = file_output;
             file_output_dir.remove_filename();
@@ -82,11 +104,12 @@ int main(int argc, char **argv) {
 
             std::ofstream output_file(file_output);
             if (!output_file.is_open()) {
-                std::cerr << "[ERROR]: Failed to open " << file_output << " for writing.\n";
+                std::cerr << "[ERROR]: Failed to open " << file_output
+                          << " for writing.\n";
                 continue;
             }
 
-            output_file << result;
+            output_file << slotted_result;
         }
     }
 
