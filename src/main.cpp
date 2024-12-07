0d4a4142d6e93ecec5abb25c66e3948ad4c8852d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-12-07 17:22:10 -0500

    Added the ability for one to either render a single file or render multiple files in a directory.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "tests.hpp"
 
 #include <fstream>
+#include <optional>
 
 using namespace std::literals::string_literals;
 namespace fs = std::filesystem;
@@ -36,8 +37,9 @@ Error render_single_file(const fs::path &in_path, const fs::path &out_path,
     return Error::OK;
 }
 
-Error render_directory(const fs::path &in_path, const fs::path &out_path) {
-    const auto config_path = in_path / "config.neng";
+Error render_directory(const fs::path &config_path,
+                       const fs::path &template_path, const fs::path &in_path,
+                       const fs::path &out_path) {
     if (!fs::exists(config_path)) {
         std::cerr << "[ERROR]: config.neng file does not exist. Make sure a "
                      "config.neng file exists in "
@@ -45,14 +47,12 @@ Error render_directory(const fs::path &in_path, const fs::path &out_path) {
         return Error::FILE_DOES_NOT_EXIST;
     }
 
-    const auto template_path = in_path / "template.html";
     if (!fs::exists(template_path)) {
         std::cerr << "[ERROR]: template.html file does not exist. Make sure a "
                      "template.html file exists in "
                   << in_path << '\n';
         return Error::FILE_DOES_NOT_EXIST;
     }
-
 
     const auto [document_config, error] =
         DocumentConfiguration::from_file(config_path.string());
@@ -106,6 +106,9 @@ int main(int argc, char **argv) {
     fs::path target_path{"./"};
     fs::path output_path{"./out"};
 
+    std::optional<fs::path> user_specified_config_path;
+    std::optional<fs::path> user_specified_template_path;
+
     for (char **arg = argv + 1; arg < argv + argc; arg++) {
         std::string_view sw_arg{*arg};
 
@@ -123,13 +126,63 @@ int main(int argc, char **argv) {
                 target_path = fs::path{*arg};
             } else if (previous_arg == "-o") {
                 output_path = fs::path{*arg};
+            } else if (previous_arg == "-c") {
+                user_specified_config_path = fs::path{*arg};
+            } else if (previous_arg == "-t") {
+                user_specified_template_path = fs::path{*arg};
             }
         }
     }
 
-    const auto result = render_directory(target_path, output_path);
-    if (result != Error::OK) {
-        std::cerr << "[ERROR]: Failed to process the directory: " << result << "\n";
+    if (fs::is_directory(target_path)) {
+        const auto config_path = target_path / "config.neng";
+        const auto template_path = target_path / "template.html";
+
+        const auto result = render_directory(config_path, template_path,
+                                             target_path, output_path);
+        if (result != Error::OK) {
+            std::cerr << "[ERROR]: Failed to process the directory: " << result
+                      << "\n";
+        }
+    } else {
+        if (!user_specified_config_path.has_value()) {
+            std::cerr << "[ERROR]: When processing a single file, you need to "
+                         "specify a configuration path.\n";
+            return EXIT_FAILURE;
+        }
+
+        if (!user_specified_template_path.has_value()) {
+            std::cerr << "[ERROR]: When processing a single file, you need to "
+                         "specify a template path.\n";
+            return EXIT_FAILURE;
+        }
+
+        const auto config_path = user_specified_config_path.value();
+        const auto template_path = user_specified_template_path.value();
+
+        const auto [document_config, error] =
+            DocumentConfiguration::from_file(config_path.string());
+        if (error != Error::OK) {
+            std::cerr << "[ERROR]: Failed to parse the configuration: " << error
+                      << '\n';
+            return EXIT_FAILURE;
+        }
+
+        const auto [document_template, error2] =
+            neng::DocumentTemplate::from_file(template_path);
+        if (error2 != Error::OK) {
+            std::cerr << "[ERROR]: Failed to parse the template: " << error
+                      << '\n';
+            return EXIT_FAILURE;
+        }
+
+        const auto error3 = render_single_file(
+            target_path, output_path, document_config, document_template);
+        if (error3 != Error::OK) {
+            std::cerr << "[ERROR]: Failed to render " << target_path << ": "
+                      << error3 << '\n';
+            return EXIT_FAILURE;
+        }
     }
 
     return 0;
